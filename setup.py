9c7f6208c83e0873c79d0adf6c0c30d331faab30
tandreas <tandreas@gmail.com>
2016-09-17 00:42:46 -0500

    Fix line endings...

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+NAME = 'pyptools'
+VERSION = '0.01'
+DESCRIPTION = 'Tools to help parsing delimited log files'
+URL = 'https://github.com/tandreas/pyptools'
+AUTHOR = 'Trevor Andreas'
+AUTHOR_EMAIL = 'tandreas@gmail.com'
+LICENSE = 'MIT'
+PACKAGES = find_packages(exclude=['tests', 'tests.*'])
+
+
+params = {
+    'name': NAME,
+    'version': VERSION,
+    'description': DESCRIPTION,
+    'url': URL,
+    'author': AUTHOR,
+    'auhtor_email': AUTHOR_EMAIL,
+    'license': LICENSE,
+    'packages': PACKAGES,
+}
+
+setup(**params)
